1f3fd9640c01dc386ab4a1708629c1f7f4a9dfb4
chen yu qiang <a@example.com>
2021-12-15 11:04:15 +0800

    44

--- src/controller.py
+++ src/controller.py
@@ -2,7 +2,7 @@
 
 import time
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist,PoseStamped
 import numpy as np
 
 now_x,now_y,now_th=0,0,0
@@ -14,15 +14,16 @@ def cb_lm(data):
 
 goal_x=2767672.7993394216
 goal_y=-352852.9162779033
+
 def cb_goalPoint(data):
     global goal_x,goal_y
-    goal_x=-352852.9162779033
-    goal_y=2767672.7993394216
+    goal_x=data.pose.position.y
+    goal_y=data.pose.position.x*(-1.0)
 
 if __name__ == '__main__':
     rospy.init_node('controller_node')
     rospy.Subscriber("/landmark", Twist,cb_lm,queue_size=1, buff_size=2**20)
-    rospy.Subscriber("/cb_goalPoint", Twist,cb_goalPoint,queue_size=1, buff_size=2**20)
+    rospy.Subscriber("/wow_utm_waypoint", PoseStamped,cb_goalPoint,queue_size=1, buff_size=2**20)
     cmd_pub=rospy.Publisher("/husky_velocity_controller/cmd_vel2",Twist,queue_size=1)
     rate=rospy.Rate(10)
 
@@ -36,13 +37,17 @@ if __name__ == '__main__':
         ang=np.arctan2((goal_y-now_y),(goal_x-now_x))-now_th
         ang=(ang+np.pi)%(2.0*np.pi)-np.pi
 
+
+
         cmd_msg=Twist()
 
-        
-        if (v_kp*dis)>0.05:
-            cmd_msg.linear.x=max(min((v_kp*dis),v_max),v_min)
-        elif (v_kp*dis)<-0.05:
-            cmd_msg.linear.x=max(min((v_kp*dis),(-1.0)*v_min),(-1.0)*v_max)
+        if abs(ang)<0.1:
+            if (v_kp*dis)>0.05:
+                cmd_msg.linear.x=max(min((v_kp*dis),v_max),v_min)
+            elif (v_kp*dis)<-0.05:
+                cmd_msg.linear.x=max(min((v_kp*dis),(-1.0)*v_min),(-1.0)*v_max)
+            else:
+                cmd_msg.linear.x=0
         else:
             cmd_msg.linear.x=0
 
