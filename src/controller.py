d0c93600e05f34a106ac5b5cfb1833fdab45d49b
Chen-Yu-Qiang <me.yuqiangchen@gmail.com>
2022-01-05 15:29:32 +0800

    PID的類與ros msg接口

--- src/controller.py
+++ src/controller.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Twist,PoseStamped
 from control_msgs.msg import PidState
 from std_msgs.msg import UInt8
 import numpy as np
+import pid_class
 s=0
 now_x,now_y,now_th=0,0,0
 def cb_lm(data):
@@ -28,59 +29,55 @@ if __name__ == '__main__':
     rospy.Subscriber("/landmark_filtered_offset", Twist,cb_lm,queue_size=1, buff_size=2**20)
     rospy.Subscriber("/wow_utm_waypoint", PoseStamped,cb_goalPoint,queue_size=1, buff_size=2**20)
     cmd_pub=rospy.Publisher("/cmd_vel",Twist,queue_size=1)
-    pid_pub=rospy.Publisher("/wow/pid",PidState,queue_size=1)
+    pid_v_pub=rospy.Publisher("/wow/pid_v",PidState,queue_size=1)
+    pid_omg_pub=rospy.Publisher("/wow/pid_omg",PidState,queue_size=1)
+
+    pid_v=pid_class.pid_controller()
+    pid_omg=pid_class.pid_controller()
+    pid_omg.output_max=1.0
+    pid_omg.output_min=-1.0
+    pid_v.output_max=0.6
+    pid_v.output_min=0.3
+    pid_v.output_zero=0.15
+
 
     error_pub=rospy.Publisher("lm/d_t_error",Twist,queue_size=1)
     achieveGoal_pub=rospy.Publisher('/wow/achieveGoal', UInt8,queue_size=1) 
     rate=rospy.Rate(10)
     
     while not rospy.is_shutdown():
-        v_kp=0.1
-        v_max=0.5
-        v_min=0.3
-        omg_kp=6
-        omg_max=0.5
+
         dis=np.sqrt((goal_y-now_y)**2+(goal_x-now_x)**2)
         ang=np.arctan2((goal_y-now_y),(goal_x-now_x))-now_th
         ang=(ang+np.pi)%(2.0*np.pi)-np.pi
+
 
         # s=0  Standby
         # s=1  Get a New Way Point and Rotating
         # s=2  Reach the Direction and Moving
 
-        cmd_msg=Twist()
+        
 
         if s==1 and abs(ang)>0.1:
-
-
-            cmd_msg.angular.z=max(min(omg_kp*ang,omg_max),(-1.0)*omg_max)
-            cmd_msg.linear.x=0
+            pid_omg.setpid(6,0,0)
+            pid_v.setpid(0,0,0)
         elif s==1 and abs(ang)<=0.1:
-            cmd_msg.angular.z=0
+            pid_omg.setpid(0,0,0)
+            pid_v.setpid(0,0,0)
             s=2
-        
         elif s==2 and (dis > 0.1) and (np.cos(ang)>0):
-            if (v_kp*dis)>0.001:
-                cmd_msg.linear.x=max(min((v_kp*dis),v_max),v_min)
-            else:
-                cmd_msg.linear.x=0
-
-            if (omg_kp*ang)>0:
-                cmd_msg.angular.z=min(omg_kp*ang,omg_max)
-            else:
-                cmd_msg.angular.z=max(omg_kp*ang,(-1.0)*omg_max)
-
-
+            pid_omg.setpid(6,0,20)
+            pid_v.setpid(0.1,0,0)
         elif s==2 and ((dis< 0.1) or (np.cos(ang)<=0)):
-            cmd_msg.linear.x=0
-            cmd_msg.angular.z=0
+            pid_omg.setpid(0,0,0)
+            pid_v.setpid(0,0,0)
             achieveGoal_msg=UInt8()
             achieveGoal_msg.data=1
             achieveGoal_pub.publish(achieveGoal_msg)
             s=0
         else:
-            cmd_msg.linear.x=0
-            cmd_msg.angular.z=0
+            pid_omg.setpid(0,0,0)
+            pid_v.setpid(0,0,0)
 
         error_msg=Twist()
         error_msg.linear.x=goal_x-now_x
@@ -89,6 +86,15 @@ if __name__ == '__main__':
         error_msg.angular.z=ang
         error_msg.angular.x=s
         error_pub.publish(error_msg)
+
+        pid_omg_msg=pid_omg.update(ang)
+        pid_v_msg=pid_v.update(dis)
+        pid_v_pub.publish(pid_v_msg)
+        pid_omg_pub.publish(pid_omg_msg)
+
+        cmd_msg=Twist()
+        cmd_msg.angular.z=pid_omg_msg.output
+        cmd_msg.linear.x=pid_v_msg.output
 
         cmd_pub.publish(cmd_msg)
         print(round(goal_y,3),round(now_y,3),round(goal_x,3),round(now_x,3),round(dis,3),ang,3),round(cmd_msg.linear.x,3),round(cmd_msg.angular.z)
---
+++ src/pid_class.py
@@ -0,0 +1,64 @@
+
+from control_msgs.msg import PidState
+
+
+class pid_controller:
+    def __inif__(self):
+        self.kp=0
+        self.ki=0
+        self.kd=0
+        self.windup=0
+
+        self.last_err=0
+        self.err=0
+        self.int_err=0
+
+        self.output=0
+
+        self.output_max=0
+        self.output_min=0
+        self.output_zero=0
+
+
+    def update(self,err):
+        self.err=err
+
+        p_error=self.kp*self.err
+
+        self.int_err=min(self.windup,max(self.int_err+self.err,self.windup*(-1.0)))
+        i_error=self.ki*self.int_err
+
+        d_error=self.kd*(self.err-self.last_err)
+
+        self.output=p_error+i_error+d_error
+        if abs(self.output)<self.output_zero:
+            self.output=0
+        else:
+            self.output=min(self.output_max,max(self.output_min,self.output))
+
+        pid_msg=PidState()
+
+        pid_msg.error=self.err
+        pid_msg.error_dot=(self.err-self.last_err)
+
+        pid_msg.p_term=self.kp
+        pid_msg.i_term=self.ki
+        pid_msg.d_term=self.kd
+
+        pid_msg.p_error=p_error
+        pid_msg.i_error=i_error
+        pid_msg.d_error=d_error
+
+        pid_msg.i_max=self.windup
+        pid_msg.i_min=self.windup*(-1.0)
+
+        pid_msg.output=self.output
+
+        self.last_err=self.err
+
+        return pid_msg
+
+    def setpid(self,kp,ki,kd):
+        self.kp=kp
+        self.ki=ki
+        self.kd=kd
