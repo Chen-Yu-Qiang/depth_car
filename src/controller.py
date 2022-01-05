34f3fd75e46cf5972ce2a069eaad71390596b9ed
Chen-Yu-Qiang <me.yuqiangchen@gmail.com>
2022-01-05 14:40:42 +0800

    1

--- src/controller.py
+++ src/controller.py
@@ -3,6 +3,7 @@
 import time
 import rospy
 from geometry_msgs.msg import Twist,PoseStamped
+from control_msgs.msg import PidState
 from std_msgs.msg import UInt8
 import numpy as np
 s=0
@@ -27,6 +28,7 @@ if __name__ == '__main__':
     rospy.Subscriber("/landmark", Twist,cb_lm,queue_size=1, buff_size=2**20)
     rospy.Subscriber("/wow_utm_waypoint", PoseStamped,cb_goalPoint,queue_size=1, buff_size=2**20)
     cmd_pub=rospy.Publisher("/cmd_vel",Twist,queue_size=1)
+    pid_pub=rospy.Publisher("/wow/pid",PidState,queue_size=1)
 
     error_pub=rospy.Publisher("lm/d_t_error",Twist,queue_size=1)
     achieveGoal_pub=rospy.Publisher('/wow/achieveGoal', UInt8,queue_size=1) 
@@ -49,10 +51,9 @@ if __name__ == '__main__':
         cmd_msg=Twist()
 
         if s==1 and abs(ang)>0.1:
-            if (omg_kp*ang)>0:
-                cmd_msg.angular.z=min(omg_kp*ang,omg_max)
-            else:
-                cmd_msg.angular.z=max(omg_kp*ang,(-1.0)*omg_max)
+
+
+            cmd_msg.angular.z=max(min(omg_kp*ang,omg_max),(-1.0)*omg_max)
             cmd_msg.linear.x=0
         elif s==1 and abs(ang)<=0.1:
             cmd_msg.angular.z=0
@@ -61,8 +62,6 @@ if __name__ == '__main__':
         elif s==2 and (dis > 0.1) and (np.cos(ang)>0):
             if (v_kp*dis)>0.05:
                 cmd_msg.linear.x=max(min((v_kp*dis),v_max),v_min)
-            elif (v_kp*dis)<-0.05:
-                cmd_msg.linear.x=max(min((v_kp*dis),(-1.0)*v_min),(-1.0)*v_max)
             else:
                 cmd_msg.linear.x=0
 
