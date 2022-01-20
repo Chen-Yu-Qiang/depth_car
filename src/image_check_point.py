8c8a104d4069c8d2061bcbcd95e90e8e883a685d
Chen-Yu-Qiang <me.yuqiangchen@gmail.com>
2022-01-20 22:21:13 +0800

    輸出影像與bag檔，校正位移量到基準點

--- src/image_check_point.py
+++ src/image_check_point.py
@@ -3,46 +3,62 @@ import csv
 import time
 
 from datetime import datetime
+from std_msgs.msg import Time
 import numpy as np
-import rospy
+import rosbag
+import tf
 import sys
-import time
+import time 
 import os
 import cv2
 from cv2 import aruco
-import matplotlib
-matplotlib.use('TKAgg')
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-if __name__=='__main__':
+
+from scipy.spatial.transform import Rotation as R
+from geometry_msgs.msg import Twist
+
+def mat_4_4_to_msg(mat):
+    q_zyx=tf.transformations.euler_from_matrix(mat[0:3,0:3], 'sxyz')
+
+    msg=Twist()
+    msg.linear.x=mat[0,3]
+    msg.linear.y=mat[1,3]
+    msg.linear.z=mat[2,3]
+    msg.angular.x=q_zyx[0]
+    msg.angular.y=q_zyx[1]
+    msg.angular.z=q_zyx[2]
+    return msg
+
+def time_to_msg(f):
+    msg=Time()
+    msg.data.secs=int(f)
+    msg.data.nsecs=(f-int(f))*(10**9)
+    return msg
+
+def fun(file_path):
     i=0
     
     # file_path=raw_input("Please enter the video path and filename : ")
-    file_path="/media/yuqiang/2CFC30D1FC309754/research data/1110121/aruco/PXL_20220118_092916642.mp4"
     while not os.path.isfile(file_path):
         print("File does not exist, please check again")
     start_time=1642497766.821
     cap = cv2.VideoCapture(file_path)
     fps=cap.get(cv2.CAP_PROP_FPS)
+    cap.set(cv2.CAP_PROP_POS_MSEC,0.0)
 
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(file_path[:-4]+'output.avi', fourcc, 60.0, (960,540))
     print("FPS = ",fps)
-    x_trj=[]
-    y_trj=[]
-    z_trj=[]
-    th_trj=[]
-    x_cam=[]
-    y_cam=[]
-    z_cam=[]
-    th_cam=[]
-
-    t=0
+    rvec_ground, tvec_ground = None,None
+    rvec_car, tvec_car = None,None
+    t=start_time
     t_list=[]
+    T_ground_list=[]
+    T_ground_car_list=[]
 
     while 1:
         success, image = cap.read()
 
         image = cv2.rotate(image, cv2.ROTATE_180)
-
         if success:
             t=t+1.0/fps
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -71,7 +87,7 @@ if __name__=='__main__':
                                     aruco.DICT_5X5_100),
                                     board_ids)
 
-            retval, rvec_ground, tvec_ground = aruco.estimatePoseBoard(corners, ids, board, camera_matrix, dist_coeffs)
+            retval, rvec_ground, tvec_ground = aruco.estimatePoseBoard(corners, ids, board, camera_matrix, dist_coeffs, rvec_ground, tvec_ground)
             if rvec_ground is None or tvec_ground is None:
                 pass
             else:
@@ -85,7 +101,14 @@ if __name__=='__main__':
                 T_ground[0:3,0:3]=rr_ground
                 T_ground[0:3,3]=tt_ground
                 T_ground[3,3]=1
-            board_corners = [np.array([[0,0.18,0],[0.18,0.18,0],[0.18,0,0],[0,0,0]],dtype=np.float32)]
+
+            # board_offset=np.array([-0.1,-0.05,0],dtype=np.float32)
+            # board_corners = [np.array([[0,0.18,0],[0.18,0.18,0],[0.18,0,0],[0,0,0]],dtype=np.float32)+board_offset]
+
+
+
+            board_offset=np.array([-0.08,0.37,0],dtype=np.float32)
+            board_corners = [np.array([[0,0,0.18],[0.18,0,0.18],[0.18,0,0],[0,0,0]],dtype=np.float32)+board_offset]
             aruco_marker_length_meters=0.5
             board_ids = np.array([[51]], dtype=np.int32)
             board = aruco.Board_create(board_corners,
@@ -93,8 +116,8 @@ if __name__=='__main__':
                                     aruco.DICT_5X5_100),
                                     board_ids)
 
-            retval, rvec_car, tvec_car = aruco.estimatePoseBoard(corners, ids, board, camera_matrix, dist_coeffs)
-            if rvec_car is None or tvec_car is None:
+            retval, rvec_car, tvec_car = aruco.estimatePoseBoard(corners, ids, board, camera_matrix, dist_coeffs, rvec_car, tvec_car)
+            if (not retval==1) or rvec_car is None or tvec_car is None:
                 pass
             else:
                 image = aruco.drawAxis( image, camera_matrix, dist_coeffs, rvec_car, tvec_car, aruco_marker_length_meters )
@@ -107,19 +130,13 @@ if __name__=='__main__':
                 T_car[3,3]=1
 
                 T_ground_car=np.dot(T_ground,T_car)
-                print(T_ground_car[0,3],T_ground_car[1,3],T_ground_car[2,3],np.arctan2(T_ground_car[0,0],T_ground_car[1,0]))
-                x_trj.append(T_ground_car[0,3])
-                y_trj.append(T_ground_car[1,3])
-                z_trj.append(T_ground_car[2,3])
-                th_trj.append(np.arctan2(T_ground_car[0,0],T_ground_car[1,0]))
 
-                x_cam.append(T_ground[0,3])
-                y_cam.append(T_ground[1,3])
-                z_cam.append(T_ground[2,3])
-                th_cam.append(np.arctan2(T_ground[0,0],T_ground[1,0]))
-                t_list.append(t)
+                t_list.append(time_to_msg(t))
+                T_ground_list.append(mat_4_4_to_msg(T_ground))
+                T_ground_car_list.append(mat_4_4_to_msg(T_ground_car))
             image2 = cv2.resize(image, (960,540), interpolation=cv2.INTER_AREA)
 
+            out.write(image2)
             cv2.imshow("b",image2)
             cv2.waitKey(1)
         else:
@@ -127,8 +144,18 @@ if __name__=='__main__':
         # time.sleep(1/fps)
     
     
-    with open(file_path[:-4]+'output.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
+    out.release()
 
-        for i in range(len(x_trj)):
-            writer.writerow([t_list[i],x_trj[i],y_trj[i],z_trj[i],th_trj[i],x_cam[i],y_cam[i],z_cam[i],th_cam[i]])
+    bag = rosbag.Bag(file_path[:-4]+'.bag', 'w')
+    for i in range(len(T_ground_car_list)):
+        bag.write('T_ground', T_ground_list[i], t=t_list[i].data)
+        bag.write('T_ground_car', T_ground_car_list[i], t=t_list[i].data)
+    bag.close()
+
+
+
+if __name__=='__main__':
+    
+    file_path="/media/yuqiang/2CFC30D1FC309754/research data/1110121/aruco_v/PXL_20220119_083950346.mp4"
+    fun(file_path)
+
