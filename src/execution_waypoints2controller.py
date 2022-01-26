76f0dbdcf9519fe962c1fe4b36d75f7ffd3fad20
Chen-Yu-Qiang <me.yuqiangchen@gmail.com>
2022-01-26 19:14:38 +0800

    檢查date_time_folder有沒有值

--- src/execution_waypoints2controller.py
+++ src/execution_waypoints2controller.py
@@ -6,8 +6,12 @@ import rospy
 from sensor_msgs.msg import NavSatFix
 
 
-a=rospy.get_param("date_time_folder")
-b=input("The datetime folder is: {} \nPlease press Enter to agree or any key to reset".format(a))
+a=rospy.get_param("date_time_folder",default="0")
+if a=="0":
+    a=input("Please enter the date-time folder of the waypoint: ~/202201")
+    a=os.path.expanduser('~')+"/202201"+a
+    rospy.set_param("date_time_folder",a)
+b=input("The date time folder is: {} \nPlease press Enter to agree or any key to reset".format(a))
 if not b=='':
     a=input("Please enter the date-time folder of the waypoint: ~/202201")
     a=os.path.expanduser('~')+"/202201"+a
@@ -18,7 +22,7 @@ import TREEDATA
 rospy.set_param("RADIUS_MAX",float(TREEDATA.R_MAX))
 rospy.set_param("RADIUS_MIN",float(TREEDATA.R_MIN))
 
-Init_tree_num=input("Please enter the trunk number to initialize the GPS offset")
+Init_tree_num=input("Please enter the trunk number to initialize the GPS offset=> ")
 rospy.set_param("Init_tree_num",Init_tree_num)
 
 print("GOGO WayPoints !")
